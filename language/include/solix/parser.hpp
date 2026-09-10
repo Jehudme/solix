89792783854e183a67e05f3d308337866ebe5d99
Jehud Othniel Metellus <jehudmetellus@outlook.com>
2026-09-10 04:34:08 -0400

    Refactor: Remove populateSymbols from parser.cpp to prepare for SemanticAnalyzer pass

--- language/include/solix/parser.hpp
+++ language/include/solix/parser.hpp
@@ -69,9 +69,6 @@ struct Node {
     size_t line = 0;
     size_t column = 0;
 
-    // Fully qualified symbol name
-    std::string symbol_name;
-
     // Pointer back to the parent node
     Node* parent_node = nullptr;
     
