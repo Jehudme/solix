7d4ff659d4eb67b5ea00be2d950f60925a2c4a48
Jehud Othniel Metellus <jehudmetellus@outlook.com>
2026-09-17 01:06:41 -0400

    refactor: Move new compiler architecture out of new folder and update includes

--- language/include/solix/new/processes/parser.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "solix/new/processes/process.hpp"
-namespace solix {
-struct Parser : public CompilationProcess {
-    using CompilationProcess::CompilationProcess;
-    void execute() override;
-};
-}
