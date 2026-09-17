08cc7aecbca7564e0aea22a9c5ce136214ab56a9
Jehud Othniel Metellus <jehudmetellus@outlook.com>
2026-09-16 20:58:21 -0400

    feat(compiler): implement new Assembler and test

---
+++ tests/src/new/test_assembler.cpp
@@ -0,0 +1,106 @@
+#include "solix/new/compilation.hpp"
+#include "solix/new/processes/lexer.hpp"
+#include "solix/new/processes/parser.hpp"
+#include "solix/new/processes/binder.hpp"
+#include "solix/new/processes/assembler.hpp"
+#include "solix/new/utilities/diagnostic.hpp"
+#include "solix/new/utilities/optcodes.hpp"
+#include <catch2/catch_test_macros.hpp>
+
+using namespace solix;
+
+inline std::string test_assemble(const std::string &code) {
+  CompilationOptions options;
+  options.log_level = CompilationOptions::LogLevel::OFF;
+  options.entry_point = "main";
+  Source src_key = std::string("test");
+  options.sources[src_key] = code;
+
+  CompilationContext context(options);
+  context.diagnostic = std::make_unique<Diagnostic>(context);
+
+  Lexer lexer(context, "Lexer");
+  lexer.execute();
+
+  Parser parser(context, "Parser");
+  parser.execute();
+
+  Binder binder(context, "Binder");
+  binder.execute();
+
+  Assembler assembler(context, "Assembler");
+  assembler.execute();
+
+  // Disassemble to string for verification
+  std::stringstream ss;
+  auto& bcode = context.bytecode;
+  for (size_t i = 0; i < bcode.size(); ) {
+      uint8_t op = bcode[i];
+      ss << opcode_to_string(op);
+      
+      switch (static_cast<OpCode>(op)) {
+          case OpCode::PUSH_CONST_I32:
+          case OpCode::GET_GLOBAL:
+          case OpCode::SET_GLOBAL:
+          case OpCode::GET_LOCAL:
+          case OpCode::SET_LOCAL:
+          case OpCode::GET_PROPERTY:
+          case OpCode::SET_PROPERTY:
+          case OpCode::JUMP:
+          case OpCode::JUMP_IF_FALSE:
+          case OpCode::JUMP_IF_TRUE:
+          case OpCode::DEFINE_NATIVE:
+          case OpCode::CALL_NATIVE: {
+              uint32_t val = (bcode[i+1] << 24) | (bcode[i+2] << 16) | (bcode[i+3] << 8) | bcode[i+4];
+              ss << " " << val;
+              i += 5;
+              
+              if (static_cast<OpCode>(op) == OpCode::DEFINE_NATIVE) {
+                  uint32_t str_len = (bcode[i] << 24) | (bcode[i+1] << 16) | (bcode[i+2] << 8) | bcode[i+3];
+                  i += 4;
+                  std::string s(reinterpret_cast<char*>(&bcode[i]), str_len);
+                  ss << " \"" << s << "\"";
+                  i += str_len;
+              }
+              break;
+          }
+          case OpCode::PUSH_CONST_I64: {
+              uint64_t val = ((uint64_t)bcode[i+1] << 56) | ((uint64_t)bcode[i+2] << 48) |
+                             ((uint64_t)bcode[i+3] << 40) | ((uint64_t)bcode[i+4] << 32) |
+                             ((uint64_t)bcode[i+5] << 24) | ((uint64_t)bcode[i+6] << 16) |
+                             ((uint64_t)bcode[i+7] << 8) | bcode[i+8];
+              ss << " " << val;
+              i += 9;
+              break;
+          }
+          case OpCode::PUSH_CONST_STRING: {
+              uint32_t str_len = (bcode[i+1] << 24) | (bcode[i+2] << 16) | (bcode[i+3] << 8) | bcode[i+4];
+              i += 5;
+              std::string s(reinterpret_cast<char*>(&bcode[i]), str_len);
+              ss << " \"" << s << "\"";
+              i += str_len;
+              break;
+          }
+          default:
+              i += 1;
+              break;
+      }
+      ss << "\n";
+  }
+  return ss.str();
+}
+
+TEST_CASE("New Assembler - Basic Variables", "[new_assembler]") {
+    std::string asm_code = test_assemble(R"(
+        public class Main {
+            public static void main() {
+                int32 x = 5;
+            }
+        }
+    )");
+    
+    // We expect some boot sequence, then main function
+    // Look for SET_LOCAL 0 and PUSH_CONST_I64 5 (since literals are 64-bit currently)
+    REQUIRE(asm_code.find("PUSH_CONST_I64 5") != std::string::npos);
+    REQUIRE(asm_code.find("SET_LOCAL") != std::string::npos);
+}
