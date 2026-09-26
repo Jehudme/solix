ee0aa7b30f6adb20ed36bfce250e40cc755e6f02
Jehud Othniel Metellus <jehudmetellus@outlook.com>
2026-09-26 06:38:29 -0400

    feat(tests): add InterfaceDeclaration test suite

---
+++ tests/statements/declarations/test_interface_declaration.cpp
@@ -0,0 +1,27 @@
+#include "test_helper.hpp"
+
+using namespace solix::test;
+
+TEST_CASE("InterfaceDeclaration - Declarations", "[declarations][interface][!mayfail]") {
+    SECTION("Case 3.1: Multiple Interface Conformance") {
+        std::string code = R"(
+interface Printable { void print(); }
+interface Serializable { void save(); }
+
+class Doc implements Printable, Serializable {
+    void print() { Console.println("print"); }
+    void save() { Console.println("save"); }
+}
+)";
+        assert_compile_success(code);
+    }
+
+    SECTION("Case 4.1: Interface Method with Body") {
+        std::string code = R"(
+interface Reader {
+    int32 read() { return 0; }
+}
+)";
+        assert_compile_error(code, "Interface methods cannot have a body");
+    }
+}
